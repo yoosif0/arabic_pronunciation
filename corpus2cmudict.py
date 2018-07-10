9077a6a1bf6ca3a0bce6b3f99ab9c207ec244f8c
Youssef Sherif <sharief@aucegypt.edu>
2018-07-10 07:13:42 +0200

    Add frequency count to outputted dic files and refactor code

--- corpus2cmudict.py
+++ corpus2cmudict.py
@@ -1,6 +1,7 @@
 import argparse
 import operator
 import re
+import collections
 
 import phonetise_Arabic
 from arutils import arabic_utils
@@ -13,6 +14,10 @@ parser.add_argument('-p', '--project-name', type=str,
 parser.add_argument('-s', '--s-tag', action='store_true',
                     help='the sentences include <s> tag')
 
+
+def calculateFrequencies(dic):
+    for word in dic:
+        dic[word]=collections.Counter(dic[word])
 
 def corpus2dictionary(corpus, project_name):
     pronunciation_dict = {}
@@ -30,17 +35,17 @@ def corpus2dictionary(corpus, project_name):
             for pronunciation in pronunciations:
                 for phone in pronunciation.split():
                     phones_list.add(phone)
-            def add(localWord, dic):
-                if localWord in dic:
-                    for pronunciation in pronunciations:
-                        dic[localWord].add(pronunciation)
-                else:
-                    dic[localWord] = set()
-                    for pronunciation in pronunciations:
-                        dic[localWord].add(pronunciation)
+
+            def addWordToDictionary(localWord, dic):
+                if localWord not in dic:
+                    dic[localWord] = []
+                dic[localWord].extend(pronunciations)
             cleaned_word = arabic_utils.remove_diacritics(word)
-            add(word, pronunciation_dict)
-            add(cleaned_word, pronunciation_dict_cleaned)
+            addWordToDictionary(word, pronunciation_dict)
+            addWordToDictionary(cleaned_word, pronunciation_dict_cleaned)
+
+    calculateFrequencies(pronunciation_dict)
+    calculateFrequencies(pronunciation_dict_cleaned)
 
     print('writing 2 dic files')
     writeFile(pronunciation_dict, proj_name + '_moshakal.dic')
@@ -56,11 +61,8 @@ def corpus2dictionary(corpus, project_name):
 def writeFile(dic, fileName):
     with open(fileName, mode='w', encoding='utf-8') as dict_writer:
         for w, phones in sorted(dic.items()):
-            for i, phone in enumerate(phones):
-                if i == 0:
-                    dict_writer.write('{}\t\t{}\n'.format(w, phone))
-                else:
-                    dict_writer.write('{}({})\t\t{}\n'.format(w, (i + 1), phone))
+            for k,v in phones.most_common():
+                dict_writer.write('{}\t\t{}\t\t{}\n'.format(w, k,v))
 
 if __name__ == '__main__':
     args = parser.parse_args()
